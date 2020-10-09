0a56bfd338ae35747ecbd8021871f22c19c3664b
enrico_scalabrino <enriscalabrino@gmail.com>
2020-10-09 10:01:47 +0200

    Trying connectors cpp driver, problem with linking.

--- main.cpp
+++ main.cpp
@@ -1,12 +1,58 @@
 #include <iostream>
+/*
+  Include directly the different
+  headers from cppconn/ and mysql_driver.h + mysql_util.h
+  (and mysql_connection.h). This will reduce your build time!
+*/
+
+#include <mysql_connection.h>
+#include <driver.h>
+#include <exception.h>
+#include <resultset.h>
+#include <statement.h>
 #include <boost/asio/ts/buffer.hpp>
 #include <boost/asio/ts/internet.hpp>
+#include <mysql_driver.h>
 #include "FileWatcher.h"
+
 
 int main() {
     std::cout << "Client Program" << std::endl;
 
+    try {
+        sql::Driver *driver;
+        sql::Connection *con;
+        sql::Statement *stmt;
+        sql::ResultSet *res;
+        /* Create a connection */
+        driver = sql::mysql::get_driver_instance();
+        con = driver->connect("tcp://127.0.0.1:3306", "root", "root");
+        /* Connect to the MySQL test database */
+        con->setSchema("test");
+
+        stmt = con->createStatement();
+        res = stmt->executeQuery("SELECT 'Hello World!' AS _message");
+        while (res->next()) {
+            std::cout << "\t... MySQL replies: ";
+            /* Access column data by alias or column name */
+            std::cout << res->getString("_message") << std::endl;
+            std::cout << "\t... MySQL says it again: ";
+            /* Access column data by numeric offset, 1 is the first column */
+            std::cout << res->getString(1) << std::endl;
+        }
+        delete res;
+        delete stmt;
+        delete con;
+
+    } catch (sql::SQLException &e) {
+        std::cout << " (MySQL error code: " << e.getErrorCode();
+        std::cout << ", SQLState: " << e.getSQLState() << " )" << std::endl;
+    }
+
+
+
     /*network programming trying */
+
 
     boost::system::error_code ec;
     boost::asio::io_context context;
