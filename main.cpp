107183a82c40e0bc8d0a2d2bfbd2501735ee7789
enrico_scalabrino <enriscalabrino@gmail.com>
2020-10-09 14:59:53 +0200

    set root and password ""..but this code must loaded to server. Only temporary code.

--- main.cpp
+++ main.cpp
@@ -21,7 +21,7 @@ int main() {
         sql::ResultSet *res;
         /* Create a connection */
         driver = get_driver_instance();
-        con = driver->connect("tcp://127.0.0.1:3306", "root", "root");
+        con = driver->connect("tcp://127.0.0.1:3306", "root", "");
         /* Connect to the MySQL test database */
         con->setSchema("test");
 
